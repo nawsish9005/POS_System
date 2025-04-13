3aa046b85bf8b892296ce99990dd64814f4d8274
nawsish <nawsish2018@gmail.com>
2025-04-13 14:34:10 +0600

    add new project

---
+++ POS_API/POS_API/Data/PosDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace POS_API.Data
+{
+    public class PosDbContext: DbContext
+    {
+        public PosDbContext(DbContextOptions<PosDbContext> options) : base(options) { }
+
+    }
+}
