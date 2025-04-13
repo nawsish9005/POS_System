8938c24ca4b68dc190325187c1bf992ccc7c98ee
nawsish <nawsish2018@gmail.com>
2025-04-13 22:11:08 +0600

    add models

--- POS_API/POS_API/Data/PosDbContext.cs
+++ POS_API/POS_API/Data/PosDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using POS_API.Model;
 using System.Numerics;
 
 namespace POS_API.Data
@@ -7,8 +8,8 @@ namespace POS_API.Data
     {
         public PosDbContext(DbContextOptions<PosDbContext> options) : base(options) { }
 
-        //public DbSet<Doctor> Doctors { get; set; }
-        //public DbSet<Patient> Patients { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Users> Users { get; set; }
         //public DbSet<Appointment> Appointments { get; set; }
         //public DbSet<MedicalRecord> MedicalRecords { get; set; }
         //public DbSet<Prescription> Prescriptions { get; set; }
---
+++ POS_API/POS_API/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace POS_API.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public DateOnly Expiry_Date { get; set; }
+        public DateOnly Manufacture_Date { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public int Quantity { get; set; }
+        public int Stock { get; set; }
+        public int UnitPrice { get; set; }
+        public int BranchId { get; set; }
+        public int CategoryId { get; set; }
+        public int SupplierId { get; set; }
+    }
+}
