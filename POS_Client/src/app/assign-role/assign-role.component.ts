78c1456632d2a226b86776b7505691d2810f59bc
nawsish <nawsish2018@gmail.com>
2025-05-14 17:42:30 +0600

    update assign role2

--- POS_Client/src/app/assign-role/assign-role.component.ts
+++ POS_Client/src/app/assign-role/assign-role.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PosService } from '../services/pos.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-assign-role',
   templateUrl: './assign-role.component.html',
   styleUrls: ['./assign-role.component.css']
 })
-export class AssignRoleComponent implements OnInit{
+export class AssignRoleComponent implements OnInit {
   assignRoleForm!: FormGroup;
   allRoles: { id: string, name: string }[] = [];
   allAssignedRoles: any[] = [];
   allUsers: string[] = [];
   message = '';
   error = '';
+  isSubmitting = false;
 
   constructor(private fb: FormBuilder, private posService: PosService) {}
 
@@ -30,24 +32,15 @@ export class AssignRoleComponent implements OnInit{
 
   getRoles() {
     this.posService.getAllRoles().subscribe(response => {
-  
       this.allRoles = response.map((r: any) => {
-        // If roles are wrapped in 'id' and 'name', unwrap them:
-        if (r.id && r.name && r.id.id && r.name.name) {
-          return {
-            id: r.id.id,
-            name: r.name.name
-          };
-        }
-        // Fallback if response is normal
+        // Normalize in case of nested structure
         return {
-          id: r.id,
-          name: r.name
+          id: r?.id?.id || r.id,
+          name: r?.name?.name || r.name
         };
       });
     });
   }
-  
 
   getAssignedRoles() {
     this.posService.getAllAssignedRoles().subscribe(data => {
@@ -57,24 +50,45 @@ export class AssignRoleComponent implements OnInit{
 
   getUsers() {
     this.posService.getAllUsers().subscribe(data => {
-      this.allUsers = data.map(u => u.userName);
+      this.allUsers = data.map((u: any) => u.userName);
     });
   }
 
   assignRole() {
     if (this.assignRoleForm.invalid) return;
 
-    this.posService.assignRole(this.assignRoleForm.value).subscribe({
+    const payload = {
+      userName: this.assignRoleForm.value.userName,
+      role: this.assignRoleForm.value.role
+    };
+
+    console.log('Assign Role Payload:', payload);
+    this.isSubmitting = true;
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    this.posService.assignRole(payload).subscribe({
       next: () => {
         this.message = 'Role assigned successfully';
         this.error = '';
         this.assignRoleForm.reset();
         this.getAssignedRoles();
+        this.isSubmitting = false;
       },
-      error: err => {
+      error: (err) => {
+        console.error('Assign Role Error:', err);
         this.error = 'Failed to assign role';
         this.message = '';
+        this.isSubmitting = false;
       }
+    });
+  }
+
+  updateAssignedRole(userName: string, roles: string[]) {
+    this.posService.updateAssignedRole({ userName, roles }).subscribe(() => {
+      this.getAssignedRoles();
     });
   }
 
